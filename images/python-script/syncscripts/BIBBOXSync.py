4b83f67a38c0f201f9d1316e2f80cb458a32a90e
reihsr <robert.reihs@gmail.com>
2017-07-26 15:04:49 +0200

    Update BIBBOXSync.py

--- images/python-script/syncscripts/BIBBOXSync.py
+++ images/python-script/syncscripts/BIBBOXSync.py
@@ -49,7 +49,7 @@ def get_filepaths(path):
 if __name__ == "__main__":
 
     elasticBaseURL = os.environ['DEBUSSY']
-    print(elasticBaseURL)
+    print("Test: " + elasticBaseURL)
     
     num_threads = 1
 
